2f685b0c2b0b2deb2a972d9f2ece13abf5aa0158
Darven23 <darvenmadhavan569@gmail.com>
2022-03-11 15:14:06 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+
+public class Location {
+
+	private int local;
+
+	public int getLocal() {
+		return local;
+	}
+
+	public void setLocal(int local) {
+		this.local = local;
+	}
+}
